bd8a489c87dce45c83225a70a78d1c885eaa0b66
f3liz <felixchen14@gmail.com>
2025-05-28 11:17:35 -0700

    constructing front end

---
+++ src/app/layout.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Layout({ children }) {
+    return(
+        <div>
+            <nav className="navbar">
+                <Link className="nav-link" href="/videos">Videos</Link>
+            </nav>
+            <main>{children}</main>
+        </div>
+    )
+}
