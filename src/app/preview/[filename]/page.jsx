71c43f2b97f4f082ddf711bfd012e9992aac3fcb
f3liz <felixchen14@gmail.com>
2025-05-29 11:36:12 -0700

    file changes

--- src/app/preview/[filename]/page.jsx
+++ src/app/preview/[filename]/page.jsx
@@ -3,7 +3,7 @@
 import { useParams } from "next/navigation";
 
 export default function PreviewPage() {
-    const { filename = '' } = useParams();
+    const { filename } = useParams();
 
     return (
         <div>
