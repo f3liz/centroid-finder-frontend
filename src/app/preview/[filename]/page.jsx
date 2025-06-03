17b263dd6dc7f77de4519e4db75458fd6a467ddd
f3liz <felixchen14@gmail.com>
2025-06-03 12:01:30 -0700

    integrated get all videos route and get thumbnail route

--- src/app/preview/[filename]/page.jsx
+++ src/app/preview/[filename]/page.jsx
@@ -1,14 +1,42 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useEffect, useState } from 'react';
 
 export default function PreviewPage() {
     const { filename } = useParams();
+    const [ imageUrl, setImageUrl ] = useState(null);
+
+    useEffect(() => {
+        fetchThumbnail();
+    }, [filename]);
+
+    const fetchThumbnail = async () => {
+        const res = await fetch(`http://localhost:3000/thumbnail/${filename}`);
+                
+        const blob = await res.blob();
+                
+        setImageUrl(URL.createObjectURL(blob));
+    };
 
     return (
         <div>
             <h1>Preview</h1>
             <p>Video being previewed: {filename}</p>
+            {imageUrl ? (
+                <div style={{ display: 'flex', gap: '2rem' }}>
+                <div>
+                    <h3>Original Frame</h3>
+                    <img src={imageUrl} alt="Thumbnail" width={320} height={240} />
+                </div>
+                <div>
+                    <h3>Binarized Preview</h3>
+                    <p>(Will update live in Step 3)</p>
+                </div>
+                </div>
+            ) : (
+                <p>Loading thumbnail...</p>
+            )}
         </div>
     )
 }
--- src/app/videos/page.jsx
+++ src/app/videos/page.jsx
@@ -1,25 +1,41 @@
 "use client";
 
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function VideoChooserPage() {
     // add api call here later
     // mock data
-    const mockData = [
-        'intro.mp4',
-        '5second.mp4',
-        '10second.mp4'
-    ]
+    // const mockData = [
+    //     'intro.mp4',
+    //     '5second.mp4',
+    //     '10second.mp4',
+    //     'Salamander5Seconds.mp4'
+    // ]
+
+    const [ videos, setVideos ] = useState([]);
+
+    useEffect(() => {
+        getVideos();
+    }, []);
+
+    const getVideos = async () => {
+        const res = await fetch('http://localhost:3000/api/videos');
+
+        const data = await res.json();
+
+        setVideos(data);
+    };
 
     return(
         <div>
             <h1>Available Videos:</h1>
             <ul>
-                {mockData.map((video, index) => (
-                    <li key={index}>
-                        <Link href={`/preview/${video}`}>{video}</Link>
+                {videos.map((filename) => (
+                    <li key={filename}>
+                        <Link href={`/preview/${filename}`}>{filename}</Link>
                     </li>
-                ))}
+                    ))}
             </ul>
         </div>
     )
