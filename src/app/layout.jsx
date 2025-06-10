4cd1d280e721213892df22e5a849c2954389d5c2
f3liz <felixchen14@gmail.com>
2025-06-10 11:52:46 -0700

    made new directory/file for our feature

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,9 @@ export default function Layout({ children }) {
             <body>
                 <div>
                     <nav className="navbar">
+                        <Link className="nav-link" href="/">Home</Link>
                         <Link className="nav-link" href="/videos">Videos</Link>
+                        <Link className="nav-link" href="/jobs">Jobs</Link>
                     </nav>
                     <main>{children}</main>
                 </div>
