1de20c587cfa0e50ade27410f0a727959a857314
Alexander Ruban <alexruban29@gmail.com>
2025-12-02 13:53:34 -0800

    Updated the cypress config file to start with te correct baseUrl for the frontend.

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here if needed
     },
   },
 });
