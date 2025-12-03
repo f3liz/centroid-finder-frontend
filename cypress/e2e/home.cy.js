9e883479125f865e077c5e72fe03da5873aa7809
Alexander Ruban <alexruban29@gmail.com>
2025-12-03 13:49:23 -0800

    Edited the home test to rely on mock videos instead of actual ones.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,18 @@
 describe('Home Page', () => {
   beforeEach(() => {
+    // Mock the /videos API so the Videos page does not crash in CI
+    cy.intercept('GET', 'http://localhost:3000/videos', {
+      statusCode: 200,
+      body: ["sample1.mp4", "sample2.mp4"] // simple mock list
+    }).as('getVideos');
+
+    // Mock the /jobs endpoint so Jobs page doesn't throw errors
+    cy.intercept('GET', 'http://localhost:3000/jobs', {
+      statusCode: 200,
+      body: []
+    }).as('getJobs');
+
+    // Visit the homepage after setting up mocks
     cy.visit('http://localhost:3001');
   });
 
